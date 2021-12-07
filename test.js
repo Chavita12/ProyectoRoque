464c2c6c01a7c0084ef4112c66856ee7bfe38d2e
Salvador <ssexys.one@gmail.com>
2021-12-06 20:44:08 -0600

    cambios1

---
+++ test.js
@@ -0,0 +1,21 @@
+var assert =require('assert');
+
+
+
+
+
+describe('probar login',function(){
+
+       
+
+    it('prueba contraseña y usuario', function(){
+
+    assert.equal('luis','luis'  );
+
+
+
+    });
+
+
+
+});
